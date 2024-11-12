73cd92525dcffc18ecc626cc54708833ec759a51
JeanRolao <162243325+JeanRolao@users.noreply.github.com>
2024-11-11 21:43:40 -0300

    678

--- perguntas.js
+++ perguntas.js
@@ -15,8 +15,9 @@ criaCartao(
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
+
 criaCartao(
     'Lingua inglesa',
-    'Como se diz OI em inglês?',
+    'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
-) 
+)
